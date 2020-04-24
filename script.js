5152f663fda29aa604bf689aa3d25cfa0dc9e8dd
Your name here <kr.shampa@yahoo.com>
2020-04-24 14:16:32 -0500

    adding second fetch

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 'use strict';
 
-function getRecipe(searchTerm) {
-    const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${searchTerm}`;
+function getRecipeVideo(searchTerm) {
+    const url = `https://yummly2.p.rapidapi.com/feeds/search?maxTotalTimeInSeconds=7200&q=${searchTerm}%20soup&start=0&maxResult=8`;
     console.log(url);
     fetch(url,
         {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "yummly2.p.rapidapi.com",
+                "x-rapidapi-key": "LgHxMTYKPpmshwhb6BYsVrPRtpvXp1pbPh5jsnDRyaXptvxh43"
+            }
+        })
+
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayVideo(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+    } 
+function getRecipe(searchTerm) {
+     const url1 =
+            `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${searchTerm}`;
+    fetch(url1, {
             "method": "GET",
             "headers": {
                 "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
@@ -22,8 +44,6 @@ function getRecipe(searchTerm) {
             $('#js-error-message').text(`Something went wrong: ${err.message}`);
         });
 }
-
-  
 function displayRecipes(responseJson) {
     console.log(responseJson);
     const results = responseJson.results;
@@ -43,18 +63,45 @@ function displayRecipes(responseJson) {
                </a>
         </li> `
         $('#results-list').append(entry);
-  
+
     }
 
 }
-function watchForm() {
+function displayVideo(responseJson){
+    const feed = responseJson.feed[0];
+    const title = feed.display.displayName;
+    const video = feed.content.videos.originalVideoUrl;
+    const steps = feed.content.preparationSteps;
 
+   $('#video').empty();
+     $('#video').removeClass('hidden');
+
+   console.log(title);
+   console.log(video);
+   console.log(steps);
+
+   const name = `<h3>${title}<h3>`
+   const embed = `<video controls width = "400px"
+                    src = '${video}'
+                     type = "video/mp4" >
+                 </video>`
+    const recipeSteps = steps.map( step => `<p>${step}<p>` );
+     $("#video").append(name);
+   /*  $('#results-video').append(embed);  */
+       $('#video').append(embed); 
+      $('#video').append(recipeSteps);   
+   /*  $('#preparation').append(recipeSteps);  */
+ 
+}
+
+
+function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
         const searchTerm = $('#js-search-term').val();
+        getRecipeVideo(searchTerm);
         getRecipe(searchTerm);
     });
-
 }
 
 $(watchForm);
